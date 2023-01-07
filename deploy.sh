80cc9d9b3f58d14b155fbd17bc49f2ae82e39e9c
bartivasa <bartivaldez@gmail.com>
2023-01-07 17:48:20 -0300

    correciones para botfront

--- deploy.sh
+++ deploy.sh
@@ -3,25 +3,39 @@
 kubectl delete namespace rasa-x
 kubectl create namespace rasa-x  
 
-helm repo add rasa-x https://rasahq.github.io/rasa-x-helm 
+kubectl delete namespace rasa
+kubectl create namespace rasa
 
-helm install generate-name \
-    --namespace rasa-x \
-    --values secret/rasa-x-values.yml \
-    rasa-x/rasa-x
+# helm repo add rasa-x https://rasahq.github.io/rasa-x-helm 
+helm repo add rasa https://helm.rasa.com
+
+# helm install --generate-name \
+#     --namespace rasa-x \
+#     --values secret/rasa-values.yml \
+#     rasa-x/rasa-x
 
 echo "Waiting for rasa-x"
 
-kubectl --namespace rasa-x \
-    wait \
-    --for=condition=available \
-    --timeout=20m \
-    --selector app.kubernetes.io/component=rasa-x \
-    deployment
+# kubectl --namespace rasa-x \
+#     wait \
+#     --for=condition=available \
+#     --timeout=20m \
+#     --selector app.kubernetes.io/component=rasa-x \
+#     deployment
 
 
-echo "Rasa-x intalled"
+echo "Rasa-x deployed"
 
 
 kubectl --namespace rasa-x get services
 
+
+echo "Deploying Rasa Opensource"
+
+helm install  --generate-name \
+   --namespace rasa-x \
+   --values secret/rasa-values.yml \
+   rasa/rasa
+
+
+echo "Rasa-opensource deployed"
